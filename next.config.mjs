7bf95c1dff6f9417bcb387f85a804b7c27bdf402
hadimughal544 <haider51012004@gmail.com>
2026-02-19 13:19:26 +0500

    ORIGIN RESOLVED

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ['http://13.61.87.194:3000'],
+  },
 };
 
 export default nextConfig;
